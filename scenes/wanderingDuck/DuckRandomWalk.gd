087688311a07cca6f8f4ef7936192931a7ae49b1
simplyorange8 <thisiskyle8@gmail.com>
2024-03-01 15:41:07 -0500

    Duck move to seed!
    Made ducks move to thrown seeds
    -Elliott Rosenberg

--- scenes/wanderingDuck/DuckRandomWalk.gd
+++ scenes/wanderingDuck/DuckRandomWalk.gd
@@ -5,6 +5,9 @@ var speed = 100
 var timer = 2.0
 var walk_ready = true
 var walking = false
+var interested = false
+var happy = false
+var seedPos = Vector2()
 var direction = Vector2(rand_range(-10, 10), rand_range(-10, 10)).normalized()
 # Declare member variables here. Examples:
 # var a = 2
@@ -21,20 +24,37 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if walk_ready:
-		direction = Vector2(rand_range(-1, 1), rand_range(-1, 1)).normalized()
-		start_timer()
-	if walking:
+	if (happy):
+		pass
+	elif (interested):
+		direction = position.direction_to(seedPos)
 		move_and_slide(direction * speed)
+	else:
+		if walk_ready:
+			direction = Vector2(rand_range(-1, 1), rand_range(-1, 1)).normalized()
+			start_timer()
+		if walking:
+			move_and_slide(direction * speed)
 func start_timer():
 	# Start the timer called WalkTimer to wait for 2 seconds
-	print("walking")
 	$WalkTimer.start(timer)
 	walking = true
 	walk_ready = false
+
+func _interest(pos):
+	seedPos = pos
+	interested = true
+
+func _happy():
+	$HappyTimer.start()
 
 func _on_WalkTimer_timeout():
 	# When the timer finishes, choose a new direction and restart the timer
 	print("changing direction")
 	walk_ready = true
 	walking = false
+
+
+func _on_HappyTimer_timeout():
+	interested = false
+	happy = true
