40ccf60b63b13c56c15cadc15d2b531b6fcc7bab
ToastedFood <76954090+ToastedFood@users.noreply.github.com>
2024-04-26 15:42:31 -0400

    Revert "Merge pull request #12 from WeeWooosr/main"
    This reverts commit d1dadcefd52cd51a10190008b94233a013ac9d5a, reversing
    changes made to e0f3c60ea04c8c2aef690481386b9c15ad436a5c.

--- scenes/wanderingDuck/DuckRandomWalk.gd
+++ scenes/wanderingDuck/DuckRandomWalk.gd
@@ -5,10 +5,10 @@ var speed = 100
 var timer = 2.0
 var walk_ready = true
 var walking = false
+var interested = false
+var happy = false
+var seedPos = Vector2()
 var direction = Vector2(rand_range(-10, 10), rand_range(-10, 10)).normalized()
-var chance = 0
-var chancing = false
-var idle = false
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -18,44 +18,43 @@ var idle = false
 func _ready():
 	walk_ready = true
 	walking = false
-	idle = false
 	randomize()
 	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if idle:
-		speed = 0
-	elif walk_ready:
-		direction = Vector2(rand_range(-1, 1), rand_range(-1, 1)).normalized()
-		chancer()
-	elif walking:
-		speed = 100
+	if (happy):
+		pass
+	elif (interested):
+		direction = position.direction_to(seedPos)
 		move_and_slide(direction * speed)
-		
-func _physics_process(delta):
-	if chancing:
-		chance += 0.01
-		var random_num = rand_range(0, 100)
-		if chance > random_num:
-			idle = false
-			walking = false
-			walk_ready = true
-	
-func chancer():
-	chance = 0
-	chancing = false
-	var random_num = rand_range(0, 100)
-	if random_num > 0 and random_num < 30:
-		print("idle")
-		walk_ready = false
-		walking = false
-		idle = true
 	else:
-		print("walking")
-		walk_ready = false
-		walking = true
-		idle = false
-	chancing = true
+		if walk_ready:
+			direction = Vector2(rand_range(-1, 1), rand_range(-1, 1)).normalized()
+			start_timer()
+		if walking:
+			move_and_slide(direction * speed)
+func start_timer():
+	# Start the timer called WalkTimer to wait for 2 seconds
+	$WalkTimer.start(timer)
+	walking = true
+	walk_ready = false
 
+func _interest(pos):
+	seedPos = pos
+	interested = true
+
+func _happy():
+	$HappyTimer.start()
+
+func _on_WalkTimer_timeout():
+	# When the timer finishes, choose a new direction and restart the timer
+	print("changing direction")
+	walk_ready = true
+	walking = false
+
+
+func _on_HappyTimer_timeout():
+	interested = false
+	happy = true
